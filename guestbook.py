59c0505daf62ed129165f72a436c6794b53fa38b
Yosuf <116045258+y0zzz@users.noreply.github.com>
2023-03-16 11:22:16 +0100

    Update guestbook.py

--- guestbook.py
+++ guestbook.py
@@ -7,22 +7,36 @@ def main():
     parser.add_argument('index', nargs='?', type=int, help='Note index')
     args = parser.parse_args()
 
-    guestbook = []
+    guestbook = load_guestbook()
 
     if args.command == 'new':
         guestbook.append(args.note)
+        save_guestbook(guestbook)
         print('Note added')
     elif args.command == 'list':
         for i, note in enumerate(guestbook):
             print(f'{i+1}. {note}')
     elif args.command == 'edit':
-        guestbook[args.index-1] = args.note
+        guestbook[-args.index] = args.note
+        save_guestbook(guestbook)
         print('Note edited')
     elif args.command == 'delete':
-        del guestbook[args.index-1]
+        del guestbook[-args.index]
+        save_guestbook(guestbook)
         print('Note deleted')
     else:
         print('Invalid command')
+
+def load_guestbook():
+    try:
+        with open('guestbook.txt', 'r') as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        return []
+
+def save_guestbook(guestbook):
+    with open('guestbook.txt', 'w') as f:
+        f.write('\n'.join(guestbook))
 
 if __name__ == '__main__':
     main()
